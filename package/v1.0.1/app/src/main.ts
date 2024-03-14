97b6a65316cd051f9ac5e0ebc0b89cdc17dc0cf1
Subham <97989643+Subham-Maity@users.noreply.github.com>
2024-03-14 07:35:54 +0530

    feat: validation pipes

--- package/v1.0.1/app/src/main.ts
+++ package/v1.0.1/app/src/main.ts
@@ -1,16 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
 import { AllExceptionsFilter } from './errors/global-exception-filter';
+import { setupGlobalPipes } from './pipes';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new AllExceptionsFilter());
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-    }),
-  );
+  setupGlobalPipes(app);
   await app.listen(3333);
 }
 bootstrap();
