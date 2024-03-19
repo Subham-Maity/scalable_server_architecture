2ed1bad9fbee169ba014897b8d63dc51a31f693b
Subham <97989643+Subham-Maity@users.noreply.github.com>
2024-03-19 09:12:53 +0530

    feat: auto send rt & at

--- package/v1.0.4/app/src/auth/strategies/at.strategy.ts
+++ package/v1.0.4/app/src/auth/strategies/at.strategy.ts
@@ -1,14 +1,21 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
-import { ExtractJwt, Strategy } from 'passport-jwt';
+import { Strategy } from 'passport-jwt';
 import { JwtPayload } from '../type';
+import { Request } from 'express';
 
 @Injectable()
 export class AtStrategy extends PassportStrategy(Strategy, 'jwt') {
   constructor(config: ConfigService) {
     super({
-      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      jwtFromRequest: (req: Request) => {
+        let token = null;
+        if (req && req.cookies) {
+          token = req.cookies['access_token'];
+        }
+        return token;
+      },
       secretOrKey: config.get<string>('JWT_LOCAL_AT_SECRET'),
     });
   }
--- package/v1.0.4/app/src/auth/strategies/rt.strategy.ts
+++ package/v1.0.4/app/src/auth/strategies/rt.strategy.ts
@@ -1,5 +1,5 @@
 import { PassportStrategy } from '@nestjs/passport';
-import { ExtractJwt, Strategy } from 'passport-jwt';
+import { Strategy } from 'passport-jwt';
 import { Request } from 'express';
 import { ForbiddenException, Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
@@ -9,16 +9,22 @@ import { JwtPayload, JwtPayloadWithRt } from '../type';
 export class RtStrategy extends PassportStrategy(Strategy, 'jwt-refresh') {
   constructor(config: ConfigService) {
     super({
-      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      jwtFromRequest: (req: Request) => {
+        let token = null;
+        if (req && req.cookies) {
+          token = req.cookies['refresh_token'];
+        }
+        return token;
+      },
       secretOrKey: config.get<string>('JWT_LOCAL_RT_SECRET'),
       passReqToCallback: true,
     });
   }
 
   validate(req: Request, payload: JwtPayload): JwtPayloadWithRt {
-    const refreshToken = req?.get('authorization')?.replace('Bearer', '').trim();
+    const refreshToken = req.cookies['refresh_token'];
 
-    if (!refreshToken) throw new ForbiddenException('Refresh token malformed');
+    if (!refreshToken) throw new ForbiddenException('Refresh token not found');
 
     return {
       ...payload,
