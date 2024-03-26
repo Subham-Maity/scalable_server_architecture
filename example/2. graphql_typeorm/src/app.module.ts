088faae1b1d6716c56f5bc33a1f01f94ad18d3dc
Subham <97989643+Subham-Maity@users.noreply.github.com>
2024-03-27 02:28:09 +0530

    feat: graphql basic setup

--- example/2. graphql_typeorm/src/app.module.ts
+++ example/2. graphql_typeorm/src/app.module.ts
@@ -1,10 +1,18 @@
+import { ApolloDriverConfig, ApolloDriver } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { BookResolver } from './book/book.resolver';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      playground: true, //make it false for the productions
+      typePaths: ['./**/*.graphql'],
+    }),
+    BookResolver,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
