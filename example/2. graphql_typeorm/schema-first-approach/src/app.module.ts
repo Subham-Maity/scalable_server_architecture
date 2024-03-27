2925e259fc7fb77318450518471f45365ae2c6ac
Subham <97989643+Subham-Maity@users.noreply.github.com>
2024-03-27 05:35:08 +0530

    feat: code first approach

--- example/2. graphql_typeorm/schema-first-approach/src/app.module.ts
+++ example/2. graphql_typeorm/schema-first-approach/src/app.module.ts
@@ -2,13 +2,18 @@ import { ApolloDriverConfig, ApolloDriver } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { BookResolver } from './book/book.resolver';
+import { join } from 'path';
 
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       playground: true, //make it false for the productions
-      typePaths: ['./**/*.graphql'],
+      autoSchemaFile: join(process.cwd(), 'src/schema.graphql'),
+      //ts-morph
+      definitions: {
+        path: join(process.cwd(), 'src/graphql.ts'),
+      },
     }),
     BookResolver,
   ],
--- example/2. graphql_typeorm/schema-first-approach/src/book/book.resolver.ts
+++ example/2. graphql_typeorm/schema-first-approach/src/book/book.resolver.ts
@@ -1,28 +1,28 @@
 import { Query, Resolver } from '@nestjs/graphql';
+import { Book } from './book.schema';
+import { Book as BookModel } from '../graphql';
 
-@Resolver('Book')
+@Resolver(() => Book)
 export class BookResolver {
-  @Query('books')
+  @Query(() => [Book], { name: 'books' }) //name set instead of getAllBooks it's now books
   getAllBooks() {
-    return [
+    const arr: BookModel[] = [
       {
         id: 1,
         title: 'Book 1',
-        author: 'Author 1',
         price: 10,
       },
       {
         id: 2,
         title: 'Book 2',
-        author: 'Author 2',
         price: 20,
       },
       {
         id: 3,
         title: 'Book 3',
-        author: 'Author 3',
         price: 30,
       },
     ];
+    return arr;
   }
 }
